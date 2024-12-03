22bac1c601c0e519374a2569d9d2e8940ff34504
Pavel <highervoodoo@gmail.com>
2024-12-03 17:59:49 +0300

    add git ignore

--- src/test/java/FelineTests.java
+++ src/test/java/FelineTests.java
@@ -5,28 +5,32 @@ import java.util.List;
 
 public class FelineTests {
 
+    private static final String EXPECTED_FAMILY = "Кошачьи";
+    private static final int DEFAULT_KITTENS_COUNT = 1;
+    private static final int SPECIFIED_KITTENS_COUNT = 3;
+    private static final List<String> EXPECTED_FOOD = List.of("Животные", "Птицы", "Рыба");
+
     @Test
     public void testEatMeat() throws Exception {
         Feline feline = new Feline();
-        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
-        assertEquals(expectedFood, feline.eatMeat());
+        assertEquals(EXPECTED_FOOD, feline.eatMeat());
     }
 
     @Test
     public void testGetFamily() {
         Feline feline = new Feline();
-        assertEquals("Кошачьи", feline.getFamily());
+        assertEquals(EXPECTED_FAMILY, feline.getFamily());
     }
 
     @Test
     public void testGetKittensDefault() {
         Feline feline = new Feline();
-        assertEquals(1, feline.getKittens());
+        assertEquals(DEFAULT_KITTENS_COUNT, feline.getKittens());
     }
 
     @Test
     public void testGetKittensWithArgument() {
         Feline feline = new Feline();
-        assertEquals(3, feline.getKittens(3));
+        assertEquals(SPECIFIED_KITTENS_COUNT, feline.getKittens(SPECIFIED_KITTENS_COUNT));
     }
 }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertThrows;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
@@ -14,23 +17,43 @@ public class LionTest {
     @Mock
     Feline felineMock;
 
+    private static final String VALID_SEX_MALE = "Самец";
+    private static final String VALID_SEX_FEMALE = "Самка";
+    private static final String INVALID_SEX = "Некорректный пол";
+
     @Test
     public void testLionGetKittens() throws Exception {
+        // Настраиваем мок
         when(felineMock.getKittens()).thenReturn(2);
-        Lion lion = new Lion("Самец", felineMock);
+
+        // Создаем объект Lion
+        Lion lion = new Lion(VALID_SEX_MALE, felineMock);
+
+        // Проверяем результат
         assertEquals(2, lion.getKittens());
     }
 
     @Test
     public void testLionGetFood() throws Exception {
+        // Настраиваем мок
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         when(felineMock.getFood("Хищник")).thenReturn(expectedFood);
-        Lion lion = new Lion("Самец", felineMock);
+
+        // Создаем объект Lion
+        Lion lion = new Lion(VALID_SEX_FEMALE, felineMock);
+
+        // Проверяем результат
         assertEquals(expectedFood, lion.getFood());
     }
 
-    @Test(expected = Exception.class)
-    public void testLionInvalidSex() throws Exception {
-        new Lion("Некорректный пол", felineMock);
+    @Test
+    public void testLionInvalidSex() {
+        // Проверяем выброс исключения с определенным сообщением
+        Exception exception = assertThrows(Exception.class, () -> {
+            new Lion(INVALID_SEX, felineMock);
+        });
+
+        // Проверяем сообщение исключения
+        assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
     }
 }
